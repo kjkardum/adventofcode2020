bd4beff96c85111aeb78b79924fe68ec13a8ac54
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-02 06:14:36 +0100

    Day 2

--- 02.py
+++ 02.py
@@ -1,2 +1,13 @@
 with open('input.txt','r') as f:
-	l=[int(i) for i in f.readlines()]
+	l=[i.split() for i in f.readlines()]
+l=[[i[0].split('-'),i[1].replace(":",""),i[2]] for i in l]
+z=0
+for i in l:
+	 if i[2].count(i[1])>=int(i[0][0]) and i[2].count(i[1])<=int(i[0][1]):
+	 	z+=1
+print('part1',z)
+z=0
+for i in l:
+	if (i[2][int(i[0][0])-1]==i[1] and not i[2][int(i[0][1])-1]==i[1]) or (not i[2][int(i[0][0])-1]==i[1] and i[2][int(i[0][1])-1]==i[1]):
+		z+=1
+print('part2',z)
