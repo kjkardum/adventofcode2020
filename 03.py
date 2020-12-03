d882ef452aadd35e1c814ecaba65e8a527c22104
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-03 12:27:22 +0100

    Day 3 V2 (One-liner)

--- 03.py
+++ 03.py
@@ -1,13 +1,1 @@
-with open('input.txt','r') as f:
-	l=[list(i.replace('\n','')) for i in f.readlines()]
-n=len(l[0])
-total=1
-for st in [[1,1],[3,1],[5,1],[7,1],[1,2]]:
-	t,k,i=0,0,0
-	while i<len(l):
-		if l[i][k]=='#':
-			t+=1
-		k=(k+st[0])%n
-		i+=st[1]
-	total*=t
-print('total',total)
+print((lambda l:(lambda m:m[0]*m[1]*m[2]*m[3]*m[4])([len([1 for i in range(0,len(l),st[1]) if l[i][i*st[0]//st[1] % len(l[0])]=='#']) for st in ([1,1],[3,1],[5,1],[7,1],[1,2])]))([list(i.replace('\n','')) for i in open('input.txt','r').readlines()]))
