eb44485a48b0863d8741b06342fbc8895169807b
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-03 06:33:51 +0100

    Day 3

--- 03.py
+++ 03.py
@@ -1,2 +1,13 @@
 with open('input.txt','r') as f:
-	l=[int(i) for i in f.readlines()]
+	l=[list(i.replace('\n','')) for i in f.readlines()]
+n=len(l[0])
+total=1
+for st in [[1,1],[3,1],[5,1],[7,1],[1,2]]:
+	t,k,i=0,0,0
+	while i<len(l):
+		if l[i][k]=='#':
+			t+=1
+		k=(k+st[0])%n
+		i+=st[1]
+	total*=t
+print('total',total)
