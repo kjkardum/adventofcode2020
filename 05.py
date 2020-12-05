3de1f0f62ebec80e1d8afa775e4d5a670193c904
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-05 06:49:00 +0100

    Day 5 V2 (One-liner)

--- 05.py
+++ 05.py
@@ -1,8 +1,1 @@
-with open('input.txt','r') as f:
-	l=[i.strip().replace('F','0').replace('B','1').replace('L','0').replace('R','1') for i in f.readlines()]
-l=[int(i[:7],2)*8+int(i[7:],2) for i in l]
-print(1,max(l))
-l.sort()
-for i in range(1,len(l)):
-	if l[i]-l[i-1]==2:
-		print(2,l[i]-1)
+print((lambda l:(max(l),max([(i-1 in l and i+1 in l and not i in l)*i for i in range(1,5000)])))((lambda l:sorted(l))([int(i[:7],2)*8+int(i[7:],2) for i in [i.strip().replace('F','0').replace('B','1').replace('L','0').replace('R','1') for i in open('input.txt','r').readlines()]])))
