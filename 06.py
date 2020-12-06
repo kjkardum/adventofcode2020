0f68af85f9a64954fe9cb20c07e66342ea3f4bfe
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-06 06:44:28 +0100

    Day 6 V2 (One-liner)

--- 06.py
+++ 06.py
@@ -1,8 +1,1 @@
-with open('input.txt','r') as f:
-	l=[i.split() for i in f.read().split('\n\n')]
-z=0
-for i in l:
-	for j in set("".join(i)):
-		if "".join(i).count(j)==len(i):
-			z+=1
-print(z)
+print(sum(len(set.intersection(*map(set,i.split())))for i in open('input.txt').read().split('\n\n')))
