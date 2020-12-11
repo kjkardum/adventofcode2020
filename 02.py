6ab008ae9f3ab71a23599b63e78c6487b09bfb58
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-11 20:47:02 +0100

    Day 2 V4 (One-liner)

--- 02.py
+++ 02.py
@@ -1,12 +1,1 @@
-with open('input.txt','r') as f:
-	l=[[list(map(int,i[0].split('-'))),i[1],i[2]] for i in [i.split() for i in f.readlines()]]
-z=0
-for i in l:
-	 if i[0][0] <= i[2].count(i[1][0]) <= i[0][1]:
-	 	z+=1
-print('part 1:',z)
-z=0
-for i in l:
-	if (i[2][i[0][0]-1]==i[1][0]) ^ (i[2][i[0][1]-1]==i[1][0]):
-		z+=1
-print('part 2:',z)
+print((lambda l:(sum(1for i in l if i[0][0]<=i[2].count(i[1][0])<=i[0][1]),sum(1for i in l if(i[2][i[0][0]-1]==i[1][0])^(i[2][i[0][1]-1]==i[1][0]))))([[list(map(int,i[0].split('-'))),i[1],i[2]]for i in[i.split()for i in open('input.txt')]]))
