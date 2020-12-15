e4d7515335f01f8c8f228fa326fb5fe675d57a2d
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-15 12:49:53 +0100

    Day 15

--- 15.py
+++ 15.py
@@ -1,2 +1,9 @@
-with open('input.txt','r') as f:
-	l=[int(i) for i in f.readlines()]
+l=list(map(int,open('input.txt').read().split(",")))
+def run(l,to=2020):
+	d={k:i for i,k in enumerate(l)}
+	n=0
+	for i in range(len(l),to-1):
+		d[n],n=i, i-d.get(n,i)
+	return n
+print(run(l[:],2020))
+print(run(l[:],30000000))
