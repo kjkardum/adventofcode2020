21f970deb3e3596c39fab69f98aefa56c5eb1b3d
Karlo Josip Kardum <kjkardum@gmail.com>
2020-12-18 21:20:56 +0100

    Day 18

--- 18.py
+++ 18.py
@@ -1,2 +1,31 @@
 with open('input.txt','r') as f:
-	l=[int(i) for i in f.readlines()]
+	l=[("("+i+")").strip().replace("(","( ").replace(")"," )").split() for i in f.readlines()]
+
+total=0
+for i in l:
+	t=[]
+	d=[t]
+	for j,k in enumerate(i):
+		if k=="(":
+			d[-1].append([])
+			d.append(d[-1][-1])
+		elif k==")":
+			z=0
+			op="+"
+			for h in d[-1]:
+				if h in "+*":
+					op=h
+				else:
+					if op=="+":
+						z+=int(h)
+					else:
+						z*=int(h)
+			d.pop(len(d)-1)
+			d[-1][-1]=str(z)
+		else:
+			d[-1].append(k)
+	total+=int(t[-1])
+print(total)
+
+
+print(sum(eval(i)for i in["("+i.replace("*",")*(")+")"for i in open("input.txt")]))
